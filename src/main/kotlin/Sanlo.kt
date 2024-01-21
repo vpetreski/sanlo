0fe36eb0ad134fd4d5dc22a91073b70b6960c485
Vanja <vanja@petreski.co>
2024-01-20 20:51:17 -0500

    CSV parsing example

--- src/main/kotlin/Sanlo.kt
+++ src/main/kotlin/Sanlo.kt
@@ -1,8 +1,23 @@
 package io.vanja
 
+import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
+import java.io.InputStream
+
 class Sanlo {
     fun execute() {
-        println("Hello World!")
+        loadCompanies()
+    }
+
+    private fun loadCompanies() {
+        csvReader().open(getCsvFile("app-companies")) {
+            readAllWithHeaderAsSequence().forEach { row: Map<String, String> ->
+                println(row["company_name"]?.trim() ?: "")
+            }
+        }
+    }
+
+    private fun getCsvFile(name: String): InputStream {
+        return this::class.java.classLoader.getResource("$name.csv")!!.openStream()
     }
 }
 
